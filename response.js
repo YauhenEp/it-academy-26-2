80e527f2eae4c66c218941e324fb69b4e8ff1d44
yauhen_bialetski <yauhen_bialetski@epam.com>
2026-05-21 17:36:03 +0200

    added function calls

--- response.js
+++ response.js
@@ -13,3 +13,8 @@ function getString() {
 function fff() {
   return 'ff'
 }
+
+console.log(add(2, 3)); // Output: 5
+console.log(divide(10, 2)); // Output: 5
+console.log(getString()); // Output: Hello World
+console.log(fff()); // Output: ff
